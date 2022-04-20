6baa2d6e3b7bec35ba4d2d7dc88ac0c6a0dc430c
Turtleb01 <datturtleboiii@gmail.com>
2022-04-20 09:02:11 +0300

    thank you hannu, very cool

---
+++ 26.js
@@ -0,0 +1,16 @@
+const { range } = require("./tools.js");
+
+function cyclelength(a) {
+  let n=1, nc=[];
+  while(true) {
+//    console.log(n + " " + a);
+//    console.log(nc);
+    if(nc.includes(n)) return(n&&(nc.length-nc.indexOf(n)));
+    nc.push(n);
+    n*=10;
+    n%=a;
+//    [n,a]=[n%a,n/a|0];
+  }
+}
+
+console.log(range(2,1000).map(e=>[e,cyclelength(e)]).reduce((v,e)=>(e[1]>v[1])?e:v,[0,0]));
